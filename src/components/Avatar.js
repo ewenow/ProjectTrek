0326f920b7cb3d8e6a440097632c390074969c06
ewelinanowicka <ewelina.k.nowicka@gmail.com>
2023-04-23 00:42:38 +0200

    updated

--- src/components/Avatar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Avatar({ src }) {
-    return (
-        <div className="d-inline-block rounded-circle overflow-hidden" style={{ width: "50px", height: "50px" }}>
-            <img src={src} alt="user avatar" style={{ width: "100%", height: "100%" }} />
-        </div>
-    );
-}
